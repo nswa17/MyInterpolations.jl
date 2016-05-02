2251c3585238707a4d4e0634511f8a807befade2
nswa17 <nswa17@gmail.com>
2016-05-02 20:10:14 +0900

    kr

--- test1.jl
+++ test1.jl
@@ -1,3 +1,17 @@
-for i in 1:10
-    println(i)
+using PyPlot
+
+function get_interpolation(xs, ys)
+  function f(x)
+    @assert xs[1] <= x <= xs[end]
+    i = searchsortedlast(xs, x)
+    @show i
+    if i == length(xs)
+      return ys[end]
+    else
+      return ys[i] + (ys[i+1] - ys[i]) / (xs[i+1] - xs[i]) * (x - xs[i])
+    end
+  end
 end
+
+f = get_interpolation([1, 2], [2, 0])
+plot(1:0.1:2, map(f, 1:0.1:2))
