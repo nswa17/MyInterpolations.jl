9bdf71afb0ea7de8212df8153969b024db4e043f
neon <neon@nakada-no-MacBook-Pro.local>
2016-05-07 12:06:34 +0900

    Revert "revised"
    This reverts commit 6883c3bf40b617992138d9d7b997e721eb812188.

--- test1.jl
+++ test1.jl
@@ -1,10 +1,10 @@
-#using PyPlot
+using PyPlot
 
 function get_interpolation(xs, ys)
   function f(x)
-    xs[1] <= x <= xs[end] || throw(DomainError())
+    @assert xs[1] <= x <= xs[end]
     i = searchsortedlast(xs, x)
-    #@show i
+    @show i
     if i == length(xs)
       return ys[end]
     else
@@ -13,18 +13,5 @@ function get_interpolation(xs, ys)
   end
 end
 
-immutable Interpolation
-  grids
-  vals
-end
-
-function Base.call(a::Interpolation, x)
-  f = get_interpolation(a.grids, a.vals)
-  return map(f, x)
-end
-
-f = Interpolation(10:0.1:20, 0:0.1:10)
-println(f(12.5))
-println(f([12, 13, 14, 15]))
-#f = get_interpolation([1, 2], [2, 0])
-#plot(1:0.1:2, map(f, 1:0.1:2))
+f = get_interpolation([1, 2], [2, 0])
+plot(1:0.1:2, map(f, 1:0.1:2))
